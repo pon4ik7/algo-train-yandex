3da8641dff30f13922b9c0fba7a8f91f9d947ca9
Rolan <muliukinrolan@mail.ru>
2026-03-19 14:13:09 +0300

    feat: add solution on competition1

--- competition1/task1/task1.go
+++ competition1/task1/task1.go
@@ -1,5 +1,39 @@
-package task1
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	in := bufio.NewReader(os.Stdin)
 
+	var n, m int
+	fmt.Fscan(in, &n, &m)
+
+	grid := make([][]rune, n)
+
+	for i := 0; i < n; i++ {
+		var s string
+		fmt.Fscan(in, &s)
+		grid[i] = []rune(s)
+	}
+
+	ans := 0
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j] == '.' {
+				if i+1 < n && grid[i+1][j] == '.' {
+					ans++
+				}
+				if j+1 < m && grid[i][j+1] == '.' {
+					ans++
+				}
+			}
+		}
+	}
+
+	fmt.Println(ans)
 }
--- competition1/task2/task2.go
+++ competition1/task2/task2.go
@@ -1,5 +1,54 @@
-package task2
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Jump struct {
+	left, right int
+	val         int
+}
+
+func NewJump(l, r, val int) Jump {
+	return Jump{left: l, right: r, val: val}
+}
 
 func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
 
+	jumps := make([]Jump, n)
+
+	var l, r, x int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&l, &r, &x)
+		jumps[i] = NewJump(l, r, x)
+	}
+
+	sort.Slice(jumps, func(i, j int) bool {
+		if jumps[i].left == jumps[j].left {
+			return jumps[i].right < jumps[j].right
+		}
+		return jumps[i].left < jumps[j].left
+	})
+
+	for i := 0; i < m; i++ {
+		var q int
+		fmt.Scan(&q)
+
+		ans := 0
+
+		for j := 0; j < n; j++ {
+			if jumps[j].left <= q && q <= jumps[j].right {
+				if (q-jumps[j].left)%2 == 0 {
+					ans += jumps[j].val
+				} else {
+					ans -= jumps[j].val
+				}
+			}
+		}
+
+		fmt.Println(ans)
+	}
 }
